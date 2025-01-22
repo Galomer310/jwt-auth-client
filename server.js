1c61e7b483558e6f42047beb2d8e82a79bf380e0
Gal Omer <galomer6708@gmail.com>
2025-01-22 16:36:45 +0200

    dependecies

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
