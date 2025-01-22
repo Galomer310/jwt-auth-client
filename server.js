fef2dcc2fb60f8f9e5501130529b26057e5f4a86
Gal Omer <galomer6708@gmail.com>
2025-01-22 16:16:53 +0200

    dist and server

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log(`running on port ${process.env.PORT || 3001}`);
+  console.log(`Running on port ${process.env.PORT || 3001}`);
 });
 
 app.use(express.static(path.join(__dirname, "/dist")));
